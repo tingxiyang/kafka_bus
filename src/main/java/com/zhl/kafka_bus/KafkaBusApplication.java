a580d701d0770736f1262dd15a08b3413ddb1ca9
zhanghongli <zhanghongli@51shebao.com>
2018-09-26 20:12:14 +0800

    init

---
+++ src/main/java/com/zhl/kafka_bus/KafkaBusApplication.java
@@ -0,0 +1,12 @@
+package com.zhl.kafka_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBusApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhl/kafka_bus/KafkaBusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhl.kafka_bus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaBusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
