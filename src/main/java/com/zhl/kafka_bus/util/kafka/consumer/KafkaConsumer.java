d50c3fa788263e80afac43c107900944f9b1adad
zhanghongli <zhanghongli@51shebao.com>
2018-09-26 20:30:18 +0800

    新增kafka配置

---
+++ src/main/java/com/zhl/kafka_bus/util/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,8 @@
+package com.zhl.kafka_bus.util.kafka.consumer;
+
+/**
+ * Created by zhl on 18/9/26 下午8:28.
+ */
+public class KafkaConsumer {
+
+}
---
+++ src/main/java/com/zhl/kafka_bus/util/kafka/producer/KafkaProducer.java
@@ -0,0 +1,7 @@
+package com.zhl.kafka_bus.util.kafka.producer;
+
+/**
+ * Created by zhl on 18/9/26 下午8:25.
+ */
+public class KafkaProducer {
+}
