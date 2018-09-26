db07b1b5355578b29a9775b0897c78fa9cf4f097
zhanghongli <zhanghongli@51shebao.com>
2018-09-26 20:38:36 +0800

    add producer

--- src/main/java/com/zhl/kafka_bus/util/kafka/producer/KafkaProducer.java
+++ src/main/java/com/zhl/kafka_bus/util/kafka/producer/KafkaProducer.java
@@ -1,7 +1,15 @@
 package com.zhl.kafka_bus.util.kafka.producer;
 
+import java.util.Properties;
+
 /**
  * Created by zhl on 18/9/26 下午8:25.
  */
 public class KafkaProducer {
+
+    //初始化配置
+    public KafkaProducer(){
+        Properties props = new Properties();
+
+    }
 }
